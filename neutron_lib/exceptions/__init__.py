9cb3930ecce1ae7e1ca71cf63550e0c6cff3d98b
Sven Rosenzweig <sven.rosenzweig@sap.com>
2024-04-25 11:00:08 +0200

    Add exception for deleting active port bindings

--- neutron_lib/exceptions/__init__.py
+++ neutron_lib/exceptions/__init__.py
@@ -816,6 +816,8 @@ class PortBindingError(NeutronException):
     message = _("Binding for port %(port_id)s on host %(host)s could not be "
                 "created or updated.")
 
+class PortBindingInStatusActive(Conflict):
+    message = _("Binding for port %(port_id)s on host %(host)s is in use. ")
 
 class ProcessExecutionError(RuntimeError):
     def __init__(self, message, returncode):
